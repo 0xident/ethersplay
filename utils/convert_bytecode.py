279fb599f29421f027c1c39c2f0c6dc2833517a8
mpy <96961800+mpysec@users.noreply.github.com>
2022-01-11 22:16:23 -0800

    Fixing up convert_bytecode script for python3

--- utils/convert_bytecode.py
+++ utils/convert_bytecode.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print "Usage: python convert_bytecode.py input.evm output.bytecode"
+        print("Usage: python convert_bytecode.py input.evm output.bytecode")
         exit(0)
 
     filename_input = sys.argv[1]
@@ -13,9 +13,9 @@ if __name__ == '__main__':
     code = f.read()
     f.close()
 
-    code = code.replace('\n', '').decode('hex')
+    code = bytes.fromhex(code.replace('\n', ''))
 
     f = open(filename_output, 'wb')
-    f.write('EVM')
+    f.write('EVM'.encode('utf-8'))
     f.write(code)
     f.close()
